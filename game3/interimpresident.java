df8c905f1ba2aba94540a4b91aa81b303470e445
tnkhrk00 <spuringlear@ezweb.ne.jp>
2021-10-28 12:03:42 +0900

    Aが自機キャラクター作成

---
+++ game3/interimpresident.java
@@ -0,0 +1,32 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class interimpresident here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class interimpresident extends Actor
+{
+    /**
+     * Act - do whatever the interimpresident wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        int x =getX();
+        int y =getY();
+           if( Greenfoot.isKeyDown( "up" ) ){
+        setLocation(x,y-3);
+    }
+        if( Greenfoot.isKeyDown( "right" ) ){
+        setLocation(x+3,y);
+    } 
+           if( Greenfoot.isKeyDown( "down" ) ){
+        setLocation(x,y+3);
+    }
+        if( Greenfoot.isKeyDown( "left" ) ){
+        setLocation(x-3,y);
+    } 
+    }    
+}
