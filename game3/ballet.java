00ec26618f1426aa6abb6dd4173b826592e0fd9b
tnkhrk00 <82485738+tnkhrk00@users.noreply.github.com>
2021-11-25 12:04:21 +0900

    Dがballetとpresident編集

--- game3/ballet.java
+++ game3/ballet.java
@@ -13,14 +13,19 @@ public class ballet extends Actor
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
   
-    
+    int time=10;
     public void act() 
     {
         // Add your action code here.
-        move(5);
+        move(1);
         Actor actor = getOneIntersectingObject( ballet.class );
-
-        int x = getX();
-        int y = getY();
+        int x =getX();
+        int y =getY();
+        
+        setLocation(x,y-90);
+        time--;
+        
+        if(time<1)
+        getWorld().removeObject(this);
     }    
 }
--- game3/interimpresident.java
+++ game3/interimpresident.java
@@ -12,6 +12,7 @@ public class interimpresident extends Actor
      * Act - do whatever the interimpresident wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    int flag_tama=0;
     public void act() 
     {
         int x =getX();
@@ -28,10 +29,15 @@ public class interimpresident extends Actor
            if( Greenfoot.isKeyDown( "left" ) ){
         setLocation(x-3,y);
     } 
-    
+    if(flag_tama>1){flag_tama--;}
+    else{    
         if(Greenfoot.isKeyDown( "e" )){
-            getWorld().addObject( new ballet(), x, y );
+            getWorld().addObject( new ballet(), x, y-90 );
+            flag_tama = 40;
+        }
     }
+    
+    
     }
             
 }
